a074183ed8a0c238a8e026c4d3b45b54e0a4f148
Ronan Collobert <ronan@collobert.com>
2014-03-01 00:28:28 +0100

    window: support for user specific size

--- window.lua
+++ window.lua
@@ -15,7 +15,7 @@ Window.__init =
       self.children = {}
       self.drv = {}
       self.flags = {nochild=false, onechild=true, hidden=false, undersized=false, dirty=false, updategeometry=false} -- DEBUG
-      self:__initAttributes({fullscreen=false, title="", resizable=false, color={r=0.9, g=0.9, b=0.9}})
+      self:__initAttributes({width=-1, height=-1, fullscreen=false, title="", resizable=false, color={r=0.9, g=0.9, b=0.9}})
       self:setAttributes({title=title})
       if attr then
          self:setAttributes(attr)
@@ -31,9 +31,6 @@ Window.onSetGeometry =
     {name="w", type="number"},
     {name="h", type="number"}},
    function(self, x, y, w, h)
-      if x >= 0 and y >= 0 and self.drv.window then
-         sdl.setWindowPosition(self.drv.window, x, y)
-      end
       if self.children[1] then
          self.children[1]:setGeometry(0, 0, w, h)
       end
@@ -43,6 +40,8 @@ Window.onSetGeometry =
 function Window:sizeRequest()
    if self.attr.resizable and self.w and self.h then
       return self.w, self.h
+   elseif self.attr.width and self.attr.height then
+      return self.attr.width, self.attr.height
    else
       if self.children[1] then
          return self.children[1]:sizeRequest()
@@ -58,14 +57,12 @@ Window.onDraw =
    function(self)
       print('geometry update?', self.flags.updategeometry)
       if self.flags.updategeometry then
-
          if not self.drv.window then
             local imgsurf = cairo.ImageSurface("rgb24", 800, 600) -- dummy
             self.drv.cr = cairo.Context(imgsurf)
          end
 
          local w, h = self:sizeRequest()
-
          self:setGeometry(-1, -1, w, h)
       end
 
@@ -78,6 +75,9 @@ Window.onDraw =
 
 function Window:setGeometry(x, y, w, h)
    if self.drv.window then
+      if x >= 0 and y >= 0 then
+         sdl.setWindowPosition(self.drv.window, x, y) -- faudrait pas juste un setPosition pour window?
+      end
       sdl.setWindowSize(self.drv.window, w, h)
    else
       local flags = 0
@@ -99,7 +99,7 @@ function Window:setGeometry(x, y, w, h)
 
    self.drv.cr = cairo.Context(cairosurf)
 
-   Widget.setGeometry(self, x, y, w, h)
+   Widget.setGeometry(self, 0, 0, w, h)
    self.flags.updategeometry = false
 end
 
